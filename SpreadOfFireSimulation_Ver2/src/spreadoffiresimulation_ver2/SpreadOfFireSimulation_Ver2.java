3a0b4cf9e9e08639a117f3d6ee2c80599ce2337b
maydonna <mayhay14532@gmail.com>
2014-11-21 03:06:34 +0700

    Spread of fire simulation version 2

---
+++ SpreadOfFireSimulation_Ver2/src/spreadoffiresimulation_ver2/SpreadOfFireSimulation_Ver2.java
@@ -0,0 +1,18 @@
+package spreadoffiresimulation_ver2;
+/**
+ *
+ * @author Anakan
+ */
+public class SpreadOfFireSimulation_Ver2 {
+
+    static GUI frame;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        frame = new GUI();
+        frame.setLocation(300, 60);
+        frame.setVisible(true);
+    }
+    
+}
